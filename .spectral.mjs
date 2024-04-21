0310bc4a5b2faf7ffe15eef274508926a6704308
nla@foryouandyourcustomers.com <nla@foryouandyourcustomers.com>
2024-04-21 17:35:50 +0200

    Initial Commit

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI0MzkwNg/spectral.js?branch_slug=main&token=4c23ccdf-7f67-40d3-b6a2-ee0111421461";
+export default { extends: ruleset };
